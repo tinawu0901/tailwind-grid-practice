7850eb8b5c0fad79c1d19891898a2663dc6588c5
IEAD\2208032 <yating.wu@iisigroup.com>
2024-12-25 22:04:48 +0800

    初版

--- src/views/time-tracking-dashboard-main.vue
+++ src/views/time-tracking-dashboard-main.vue
@@ -1,4 +1,33 @@
-<template>123123</template>
+<template>
+  <div class="bg-black w-full h-dvh" style="border: 1px red solid">
+    <div
+      class="grid grid-rows-2 gap-5 grid-cols-4 place-content-center h-96 w-4/5 absolute inset-0 m-auto max-sm:grid-cols-1"
+      style="border: 1px red solid"
+    >
+      <div class="row-span-2" style="border: 1px red solid">
+        <div class="bg-indigo-500 w-full h-4/5 rounded text-white">
+          <div style="border: 1px red solid" class="w-full h-1/2 m-auto">
+            <img
+              alt="Vue logo"
+              src="@/assets/images/time-tracking-dashboard-main/image-jeremy.png"
+              class="w-20 h-20 border-white border-2 rounded-full ml-4 mt-4"
+            />
+          </div>
+          <div class="text-left ml-3 text-sm" style="border: 1px red solid">
+            Report for
+            <p class="text-2xl text-wrap">Jeremy<br />Robson</p>
+          </div>
+        </div>
+      </div>
+      <div class="" style="border: 1px red solid">02</div>
+      <div class="" style="border: 1px red solid">03</div>
+      <div class="" style="border: 1px red solid">04</div>
+      <div class="" style="border: 1px red solid">05</div>
+      <div class="" style="border: 1px red solid">06</div>
+      <div class="" style="border: 1px red solid">07</div>
+    </div>
+  </div>
+</template>
 <script setup lang="ts">
 import Vue from "vue";
 </script>
